83c258cff5c800ab108fa8aee98f9fedbb94aefe
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-11-03 08:38:55 +0000

    Fix modulo operator

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from tokenizer.tokenizer import Tokenizer, print_token_stream
 
 if __name__ == '__main__':
     _t = Tokenizer('123a_abc+764 =- obj/62 *+9 az++ or s=90-7 and q+=-8 or w/=98-i\n'
-                   ' bc*=8 or ee+q  -=4gt9')
+                   ' bc*=8 or ee+q  -=4gt9 % +abc xor u%=99-3*q/d')
     _t.tokenize()
     print_token_stream(_t.tokens, True)
--- tokenizer/const.py
+++ tokenizer/const.py
@@ -2,5 +2,5 @@ import string
 
 
 NAME_CHARS = string.ascii_letters + string.digits + '_'
-OP_CHARS = '+-*/='
+OP_CHARS = '+-*/%='
 VALID_CHARS = NAME_CHARS + OP_CHARS
